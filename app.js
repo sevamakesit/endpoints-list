3b57804a2e370a2e9fb00babaf54c1c3b0cc5a6d
Seva Rybakov <jumpincode@gmail.com>
2022-02-11 06:00:09 +0800

    first working version

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from 'express'
-import ReviewAppsManager from './review-apps-manager.js'
+import ReviewAppsManager from './lib/review-apps-manager.js'
 
 const manager = new ReviewAppsManager()
 
 const app = express()
+
+app.get('/urls', async (req, res) => {
+  const urls = await manager.getAllAppUrls()
+  res.json(urls)
+})
 
 export default app
--- test/lib/review-apps-manager.test.js
+++ test/lib/review-apps-manager.test.js
@@ -1,13 +1,17 @@
 import ReviewAppManager from '../../lib/review-apps-manager.js'
+import fetchMock from 'jest-fetch-mock'
+fetchMock.enableMocks()
 
 describe('ReviewAppManager', () => {
   const manager = new ReviewAppManager()
 
   beforeEach(async () => {
+    fetch.resetMocks()
+    fetch.doMock()
     await manager.clearAll()
   })
 
-  describe("management of keys", () => {
+  describe("management of app names", () => {
     it("can add and remove app names", async () => {
       await manager.addReviewAppName('foo1')
       await manager.addReviewAppName('foo2')
@@ -15,6 +19,23 @@ describe('ReviewAppManager', () => {
 
       const reviewApps = await manager.getReviewAppNames()
       expect(reviewApps).toEqual(['foo2'])
+    })
+
+    it("uses redis set, so there is no duplicates", async () => {
+      await manager.addReviewAppName('foo1')
+      await manager.addReviewAppName('foo1')
+      await manager.addReviewAppName('foo2')
+
+      const reviewApps = await manager.getReviewAppNames()
+      expect(reviewApps.sort()).toEqual(['foo1', 'foo2'])
+    })
+  })
+
+  describe("#getReviewAppUrls", () => {
+    it("returns correct herokupp urls", async () => {
+      await manager.addReviewAppName('test-app')
+      const urls = await manager.getReviewAppUrls()
+      expect(urls).toEqual(['https://test-app.herokuapp.com'])
     })
   })
 })
