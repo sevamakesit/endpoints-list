e1a9a098f7b6a454acb6c751c5548f5bc0bb6a6c
Seva Rybakov <jumpincode@gmail.com>
2022-02-11 08:42:59 +0800

    Add basic auth

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from 'express'
+import basicAuth from 'express-basic-auth'
 import ReviewAppsManager from './lib/review-apps-manager.js'
 
 const manager = new ReviewAppsManager()
 
 const app = express()
+
+app.use(basicAuth({
+  users: { 'zipline_device': process.env.SECRET }
+})
 
 app.get('/urls', async (req, res) => {
   const urls = await manager.getAllAppUrls()
@@ -11,19 +16,11 @@ app.get('/urls', async (req, res) => {
 })
 
 app.post('/review-apps/:name?', async (req, res) => {
-  if (req.query.secret !== process.env.SECRET) {
-    res.status(403).json({status: 'go away'})
-    return
-  }
   const urls = await manager.addReviewAppName(req.params.name)
   res.json({status: 'ok'})
 })
 
 app.delete('/review-apps/:name?', async (req, res) => {
-  if (req.query.secret !== process.env.SECRET) {
-    res.status(403).json({status: 'go away'})
-    return
-  }
   const urls = await manager.removeReviewAppName(req.params.name)
   res.json({status: 'ok'})
 })
